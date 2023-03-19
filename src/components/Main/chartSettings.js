cede020f728e2bb5033a4c050e5e4a9950c41450
Kirill Troitsky <Kirill.Troitsky@leomax.ru>
2023-03-19 20:51:53 +0300

    fixed chart

--- src/components/Main/chartSettings.js
+++ src/components/Main/chartSettings.js
@@ -1,29 +1,72 @@
 export const options = {
+  maintainAspectRatio: false,
   responsive: true,
   plugins: {
     legend: {
       display: false,
     },
+    tooltip: {
+      enabled: false,
+    },
   },
-  onHover: (event) => (event.native.target.style.cursor = 'pointer'),
+  scales: {
+    x: {
+      stacked: true,
+      grid: {
+        display: false,
+      },
+      ticks: {
+        font: {
+          size: 16,
+          weight: 700,
+        },
+        color: '#676767',
+      },
+      border: {
+        display: false,
+      },
+    },
+    y: {
+      display: false,
+      stacked: true,
+    },
+  },
+  onHover: (event, chartElement) => {
+    if (chartElement.length == 1) {
+      event.native.target.style.cursor = 'pointer';
+    }
+  },
 };
 
-export const labels = [
-  { grade: 1, label: 'Ужасно', color: '#C22126' },
-  { grade: 2, label: 'Плохо', color: '#DB7805' },
-  { grade: 3, label: 'Нормально', color: '#FFC700' },
-  { grade: 4, label: 'Хорошо', color: '#8DCF00' },
-  { grade: 5, label: 'Отлично', color: '#09B83E' },
-];
+export const labels = [1, 2, 3, 4, 5];
 
 export const generateData = (labels, reviews) => {
   return {
-    labels: labels.map((item) => item.label),
+    labels,
     datasets: [
       {
-        data: labels.map((item) => reviews?.reduce((prevValue, review) => (review.ratings === item.grade ? (prevValue += 1) : prevValue), 0)),
-        borderWidth: 0,
-        backgroundColor: labels.map((item) => item.color),
+        data: labels.map((item) =>
+          reviews.reduce(
+            (prevValue, review) => (review.ratings === item ? (prevValue += 1) : prevValue),
+            0
+          )
+        ),
+        backgroundColor: '#FCAF17',
+        hoverBackgroundColor: '#FBD620',
+        borderRadius: 6,
+        cursor: 'pointer',
+      },
+      {
+        data: labels.map(
+          (item) =>
+            reviews.length -
+            reviews.reduce(
+              (prevValue, review) => (review.ratings === item ? (prevValue += 1) : prevValue),
+              0
+            )
+        ),
+        backgroundColor: '#F3F3F3',
+        borderRadius: 6,
       },
     ],
   };
